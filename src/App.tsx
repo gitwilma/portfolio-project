b2ff9b35db4ab1529184c6dd8cceb10b5f35685a
gitwilma <wilma.haakansson@gmail.com>
2025-02-20 11:04:47 +0100

    added footer with basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <header>
-        <h1 className="p-2 text-purple-300">Hello World</h1>
+        <h1 className="p-2 text-black text-9xl">Welcome to my portfolio</h1>
       </header>
-      <span>right</span>
-      <main>
+      <main className="flex-grow container mx-auto px-4">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
