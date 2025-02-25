e28db2014b4b1b3cfc6855a7feee6d1d32b23b04
gitwilma <wilma.haakansson@gmail.com>
2025-02-25 13:47:54 +0100

    styling about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,20 @@
 export default function About() {
   return (
-    <div className="px-10 py-30 max-w-2xl">
+    <div className="px-10 py-10 max-w-2xl font-serif">
       <h2 className="text-5xl py-5">About me</h2>
       <p>
         Hi! My name is Wilma and I am a frontend developer. With skills in
         modern web technologies like HTML, CSS, JavaScript, TypeScript, and
         React — as well as version control with Git/GitHub — I enjoy bringing
         ideas to life and to craft user-friendly and functional web experiences.
-        I have a keen eye for detail and a problem-solving mindset, shaped by
-        both my education in web development and previous experience in
+        <br />
+        <br />I have a keen eye for detail and a problem-solving mindset, shaped
+        by both my education in web development and previous experience in
         structured, team-oriented environments. Always eager to learn, grow, and
         refine my skills, I’m excited to take the next step in my journey as a
         frontend developer.
       </p>
-      <div className="absolute top-[250px] left-[900px]">
+      <div className="absolute top-[200px] left-[900px]">
         <img
           src="/assets/cv-image.png"
           alt="CV Image"
