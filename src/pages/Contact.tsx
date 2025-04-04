82a92a6e44758142bbf28d1cecbd65a1c8d2cade
gitwilma <wilma.haakansson@gmail.com>
2025-04-04 14:31:25 +0200

    responsive contact page wip

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -23,19 +23,19 @@ export default function Contact() {
       {/* Formulär och kontaktinfo som två cirklar */}
       <div className="flex flex-col sm:flex-row gap-8 w-full justify-center items-center">
         {/* Formulär cirkel */}
-        <div className="flex flex-col items-center justify-center w-140 h-140 rounded-full bg-[#000000] shadow-md p-4">
+        <div className="flex flex-col items-center justify-center w-56 h-56 sm:w-64 sm:h-64 md:w-96 md:h-96 rounded-full bg-[#000000] shadow-md p-4">
           <form
             onSubmit={handleSubmit}
-            className="space-y-4 w-full text-center"
+            className="space-y-2 w-full text-center"
           >
-            <h3 className="text-xl font-semibold font-[Poppins] text-[#D8B17D]">
+            <h3 className="text-xs sm:text-sm md:text-lg font-semibold font-[Poppins] text-[#D8B17D]">
               Contact Me
             </h3>
 
             <div>
               <label
                 htmlFor="name"
-                className="block text-md font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
+                className="block text-xxs sm:text-xs md:text-sm font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
               >
                 Your Name
               </label>
@@ -44,7 +44,7 @@ export default function Contact() {
                 id="name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                className="w-full px-3 py-2 bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
+                className="w-full px-2 py-1 text-xxs sm:text-xs md:text-sm bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
                 placeholder="Enter your name"
               />
             </div>
@@ -52,7 +52,7 @@ export default function Contact() {
             <div>
               <label
                 htmlFor="email"
-                className="block text-md font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
+                className="block text-xxs sm:text-xs md:text-sm font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
               >
                 Your Email
               </label>
@@ -61,7 +61,7 @@ export default function Contact() {
                 id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="w-full px-3 py-2 bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
+                className="w-full px-2 py-1 text-xxs sm:text-xs md:text-sm bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
                 placeholder="Enter your email"
               />
             </div>
@@ -69,7 +69,7 @@ export default function Contact() {
             <div>
               <label
                 htmlFor="message"
-                className="block text-md font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
+                className="block text-xxs sm:text-xs md:text-sm font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
               >
                 Your Message
               </label>
@@ -77,13 +77,13 @@ export default function Contact() {
                 id="message"
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
-                className="w-full px-3 py-2 bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
+                className="w-full px-2 py-1 text-xxs sm:text-xs md:text-sm bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
                 placeholder="How can I help you?"
               />
             </div>
 
             {formStatus && (
-              <div className="text-right text-lg font-semibold text-[#F4E1C4]">
+              <div className="text-right text-xxs sm:text-xs md:text-lg font-semibold text-[#F4E1C4]">
                 <p>{formStatus}</p>
               </div>
             )}
@@ -91,7 +91,7 @@ export default function Contact() {
             <div className="flex justify-center">
               <button
                 type="submit"
-                className="px-8 py-3 bg-pink-400 text-white rounded-full hover:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-[#D8B17D]"
+                className="px-6 py-2 bg-pink-400 text-white rounded-full hover:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-[#D8B17D]"
               >
                 Send
               </button>
@@ -100,11 +100,11 @@ export default function Contact() {
         </div>
 
         {/* Kontaktinformation cirkel */}
-        <div className="flex flex-col items-center justify-center w-80 h-80 rounded-full bg-[#000000] shadow-md p-4">
-          <h3 className="text-2xl text-center font-bold font-[Poppins] text-[#D8B17D]">
+        <div className="flex flex-col items-center justify-center w-56 h-56 sm:w-64 sm:h-64 md:w-96 md:h-96 rounded-full bg-[#000000] shadow-md p-4">
+          <h3 className="text-xxs sm:text-xs md:text-2xl text-center font-bold font-[Poppins] text-[#D8B17D]">
             Contact Information
           </h3>
-          <p className="text-lg text-center font-[Poppins] text-[#D8B17D] p-4">
+          <p className="text-xxs sm:text-xs md:text-lg text-center font-[Poppins] text-[#D8B17D] p-4">
             📧 wilma.haakansson@gmail.com <br />
             📞 +46 72 701 2121 <br />
             📍 Gothenburg, Sweden
