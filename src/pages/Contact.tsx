d6bf89914c3e47783e7b6c8b3b36db5820d9d5d8
gitwilma <wilma.haakansson@gmail.com>
2025-04-05 12:01:33 +0200

    contact page responsive

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,7 +6,6 @@ export default function Contact() {
   const [message, setMessage] = useState("");
   const [formStatus, setFormStatus] = useState("");
 
-  // Hantera form submission
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
@@ -20,22 +19,18 @@ export default function Contact() {
 
   return (
     <div className="flex flex-col items-center px-4 py-10">
-      {/* Formulär och kontaktinfo som två cirklar */}
       <div className="flex flex-col sm:flex-row gap-8 w-full justify-center items-center">
-        {/* Formulär cirkel */}
-        <div className="flex flex-col items-center justify-center w-56 h-56 sm:w-64 sm:h-64 md:w-96 md:h-96 rounded-full bg-[#000000] shadow-md p-4">
-          <form
-            onSubmit={handleSubmit}
-            className="space-y-2 w-full text-center"
-          >
-            <h3 className="text-xs sm:text-sm md:text-lg font-semibold font-[Poppins] text-[#D8B17D]">
+        {/* Form circle */}
+        <div className="flex flex-col items-center justify-center w-76 h-76 sm:w-84 sm:h-84 md:w-96 md:h-96 rounded-full bg-[#000000] shadow-md p-4">
+          <form onSubmit={handleSubmit} className="space-y-2 text-center">
+            <h3 className="text-sm sm:text-sm md:text-lg font-semibold font-[Poppins] text-[#D8B17D]">
               Contact Me
             </h3>
 
             <div>
               <label
                 htmlFor="name"
-                className="block text-xxs sm:text-xs md:text-sm font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
+                className="block text-sm sm:text-xs md:text-sm font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
               >
                 Your Name
               </label>
@@ -44,7 +39,7 @@ export default function Contact() {
                 id="name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                className="w-full px-2 py-1 text-xxs sm:text-xs md:text-sm bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
+                className="w-full px-1 py-1 text-xs sm:text-xs md:text-sm bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-2 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
                 placeholder="Enter your name"
               />
             </div>
@@ -52,7 +47,7 @@ export default function Contact() {
             <div>
               <label
                 htmlFor="email"
-                className="block text-xxs sm:text-xs md:text-sm font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
+                className="block text-sm sm:text-xs md:text-sm font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
               >
                 Your Email
               </label>
@@ -61,7 +56,7 @@ export default function Contact() {
                 id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="w-full px-2 py-1 text-xxs sm:text-xs md:text-sm bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
+                className="w-full px-2 py-1 text-xs sm:text-xs md:text-sm bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
                 placeholder="Enter your email"
               />
             </div>
@@ -69,7 +64,7 @@ export default function Contact() {
             <div>
               <label
                 htmlFor="message"
-                className="block text-xxs sm:text-xs md:text-sm font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
+                className="block text-sm sm:text-xs md:text-sm font-semibold mb-2 font-[Poppins] text-[#F4E1C4]"
               >
                 Your Message
               </label>
@@ -77,7 +72,7 @@ export default function Contact() {
                 id="message"
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
-                className="w-full px-2 py-1 text-xxs sm:text-xs md:text-sm bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
+                className="w-full px-2 py-1 text-xs sm:text-xs md:text-sm bg-[#F4E1C4] rounded-md focus:outline-none focus:ring-4 focus:ring-[#D8B17D] font-[Poppins] text-black opacity-80"
                 placeholder="How can I help you?"
               />
             </div>
@@ -99,8 +94,8 @@ export default function Contact() {
           </form>
         </div>
 
-        {/* Kontaktinformation cirkel */}
-        <div className="flex flex-col items-center justify-center w-56 h-56 sm:w-64 sm:h-64 md:w-96 md:h-96 rounded-full bg-[#000000] shadow-md p-4">
+        {/* Info circle */}
+        <div className="flex flex-col items-center justify-center w-76 h-76 sm:w-64 sm:h-64 md:w-96 md:h-96 rounded-full bg-[#000000] shadow-md p-4">
           <h3 className="text-xxs sm:text-xs md:text-2xl text-center font-bold font-[Poppins] text-[#D8B17D]">
             Contact Information
           </h3>
